2b8a88ad1022526443e758c9e94118eb693e4a80
Yunseong <dev.yunseong@gmail.com>
2025-07-15 15:14:29 +0900

    refactor Repository - JpaRepository로 수정, 메서드 이름 jpa 스럽게 수정, 필요 없는 메서드 삭제

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -1,64 +1,11 @@
 package gift.repository;
 
-import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
-import gift.domain.Member;
+import gift.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-@Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
-    private final JdbcClient jdbcClient;
-
-    public MemberRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
-
-    private static final String SAVE_MEMBER = """
-            INSERT INTO members(email, passwordHash) VALUES
-            (:email, :passwordHash);
-            """;
-    private static final String FIND_MEMBER_BY_EMAIL = """
-            SELECT id, email, passwordHash
-            FROM members
-            WHERE email = :email;
-            """;
-    private static final String FIND_MEMBER_BY_ID = """
-            SELECT id, email, passwordHash
-            FROM members
-            WHERE id = :id;
-            """;
-
-    public Optional<Number> saveMember(Member member) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcClient.sql(SAVE_MEMBER)
-                .param("email", member.getEmail())
-                .param("passwordHash", member.getPasswordHash())
-                .update(keyHolder);
-        return Optional.ofNullable(keyHolder.getKey());
-    }
-
-    public Optional<Member> findMemberByEmail(String email) {
-        return jdbcClient.sql(FIND_MEMBER_BY_EMAIL)
-                .param("email", email)
-                .query((rs, numOfRows) ->
-                        new Member(
-                                rs.getLong("id"),
-                                rs.getString("email"),
-                                rs.getString("passwordHash")
-                        )).optional();
-    }
-
-    public Optional<Member> findMemberById(long id) {
-        return jdbcClient.sql(FIND_MEMBER_BY_ID)
-                .param("id", id)
-                .query((rs, numOfRows) ->
-                        new Member(
-                                rs.getLong("id"),
-                                rs.getString("email"),
-                                rs.getString("passwordHash")
-                        )).optional();
-    }
+    Optional<MemberEntity> findMemberByEmail(String email);
+    Optional<MemberEntity> findMemberById(long id);
 }
